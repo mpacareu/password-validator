0bfd878c0fe088a33c1608fc81d292980018cb2e
Casas, Arnau <arnau.casas@sky.uk>
2026-05-04 11:16:55 +0100

    Arnau Casas

--- src/main/java/com/bskyb/quarks/PasswordValidator.java
+++ src/main/java/com/bskyb/quarks/PasswordValidator.java
@@ -1,7 +1,50 @@
 package com.bskyb.quarks;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PasswordValidator {
     public boolean isValid(String password) {
-        throw new RuntimeException("Implement something.");
+        var rulesFollowed = 0;
+        HashMap<String, Boolean> rules = new HashMap<>();
+
+        //Assignation of rules
+        rules.put("8 characters", false);
+        rules.put("a capital letter", false);
+        rules.put("a lowercase", false);
+        rules.put("a number", false);
+        rules.put("an underscore", false);
+
+        //Check of the 8 characters
+        if (password.length() >= 8) {
+            rules.put("8 characters", true);
+        }
+
+        //Check of a number, lowercase and capital letter
+        for (char c : password.toCharArray()) {
+            if (Character.isDigit(c)) {
+                rules.put("a number", true);
+            }
+            if (Character.isLowerCase(c)) {
+                rules.put("a lowercase", true);
+            }
+            if (Character.isUpperCase(c)) {
+                rules.put("a capital letter", true);
+            }
+        }
+
+        //Check for the underscore
+        if (password.contains("_")) {
+            rules.put("an underscore", true);
+        }
+
+        for (Map.Entry<String, Boolean> rule : rules.entrySet()) {
+            if (rule.getValue() == false) {
+                System.out.println("Your password needs " + rule.getKey() + ".");
+            }else{
+                ++rulesFollowed;
+            }
+        }
+        return rulesFollowed == rules.size();
     }
 }
--- src/test/java/com/bskyb/quarks/PasswordValidatorTest.java
+++ src/test/java/com/bskyb/quarks/PasswordValidatorTest.java
@@ -2,14 +2,29 @@ package com.bskyb.quarks;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class PasswordValidatorTest {
 
     @Test
-    public void test() {
-        assertTrue(true);
+    public void correctPassword() {
+        PasswordValidator validator = new PasswordValidator();
+        assertTrue(validator.isValid("Hola_567"));
+        assertTrue(validator.isValid("Nube_4AltaX"));
+        assertTrue(validator.isValid("Clave_9Segura"));
+        assertTrue(validator.isValid("Sol_2026Az"));
+    }
+    @Test
+    public void wrongPassword() {
+        PasswordValidator validator = new PasswordValidator();
+        assertFalse(validator.isValid("pepe_1A"));
+        assertFalse(validator.isValid("sinnumero_Aa"));
+        assertFalse(validator.isValid("SIN_123456"));
+        assertFalse(validator.isValid("clave123Aa"));
+        assertFalse(validator.isValid(" "));
+
     }
 
 }
